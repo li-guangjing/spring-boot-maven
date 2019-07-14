aff15b2b68c66683a4f55d540b82db828c62be06
Benjamin-GitHub <648822980@qq.com>
2019-07-14 22:14:31 +0800

    1,pom文件添加starter、web（localhost:8080即可访问）依赖，以及plugin打包配置 2,添加启动类，在src/main/java目录下，关注点在enableconfigration注解上

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,5 +4,19 @@
  * @Date 2019/7/14 21:30
  * @Version 1.0
  */
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@EnableAutoConfiguration
 public class Test {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Test.class, args);
+    }
 }

829ae4d2da39ce9c9d0902c5d22c18f0329c8ea2
Benjamin-GitHub <648822980@qq.com>
2019-07-14 22:46:16 +0800

    1,pom文件添加test依赖 2,添加测试类

--- src/test/java/TestCase.java
+++ src/test/java/TestCase.java
@@ -1,8 +1,35 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
 /**
  * @Decription
  * @Authur Benjamin
  * @Date 2019/7/14 21:30
  * @Version 1.0
  */
+@RunWith(SpringRunner.class)
+@ContextConfiguration
 public class TestCase {
+
+
+    @Test
+    public void test(){
+        System.out.println("Hello test.");
+    }
+
+    @Before
+    public void before(){
+        System.out.println("Befor hello test.");
+    }
+
+    @After
+    public void after(){
+        System.out.println("After hello test.");
+    }
 }
